574ebd24e9643e51de40d5f5633fc27e10114716
Dwight McMillan <dwightmcmillan76@gmail.com>
2026-03-08 13:12:35 -0400

    Update run_epic.py

--- examples/run_epic.py
+++ examples/run_epic.py
@@ -1,8 +1,7 @@
-# examples/run_epic.py
 from src.epic_core import EpicCore
 
 if __name__ == "__main__":
     epic = EpicCore()
-    query = "What is the current president of the United States?"
+    query = "What is the capital of France?"
     response = epic.process_query(query)
     print("EPIC Response:", response)
